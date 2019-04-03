4facc9e2df98c6c4222931d9bb76d48106325152
vmsilvamolina <vmsilvamolina@gmail.com>
2019-04-03 17:31:02 -0300

    fix the commands with the new Az module and add new draft file

--- PowerShellDeployExample.ps1
+++ PowerShellDeployExample.ps1
@@ -1,9 +1,9 @@
 # Login to Azure
-Login-AzureRmAccount
+Login-AzAccount
 
 # Resource Group
 $rg = "myappinfra-rg"
-New-AzureRmResourceGroup -Name $rg -Location westeurope
+New-AzResourceGroup -Name $rg -Location westeurope
 
 # Deploy via Resource Group Deployment (replace with the correct name of template and parameters file)
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -Mode Complete -TemplateFile ".\azuredeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json"
+New-AzResourceGroupDeployment -ResourceGroupName $rg -Mode Complete -TemplateFile ".\azuredeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json"
