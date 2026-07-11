70bcb119d6ae194c98008205157a03fa3b02e7d3
Victor Silva <vmsilvamolina@gmail.com>
2026-07-10 22:17:32 -0300

    refactor(deploy): update PowerShellDeployExample to deploy bicep

--- PowerShellDeployExample.ps1
+++ PowerShellDeployExample.ps1
@@ -1,9 +1,33 @@
-# Login to Azure
-Login-AzAccount
+# Connect to Azure
+Connect-AzAccount
 
 # Resource Group
 $rg = "myappinfra-rg"
-New-AzResourceGroup -Name $rg -Location westeurope
+$location = "westeurope"
+New-AzResourceGroup -Name $rg -Location $location -Force
 
-# Deploy via Resource Group Deployment (replace with the correct name of template and parameters file)
-New-AzResourceGroupDeployment -ResourceGroupName $rg -Mode Complete -TemplateFile ".\azuredeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json"
+# Baseline: App Service Plan + Web App
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $rg `
+  -TemplateFile ".\bicep\webapp.bicep" `
+  -webAppName "myapp"
+
+# Web App + Redis, secretos vía Key Vault + managed identity
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $rg `
+  -TemplateFile ".\bicep\webapp-redis.bicep" `
+  -appName "myapp"
+
+# Web App detrás de Front Door Premium + WAF, origin lock-down
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $rg `
+  -TemplateFile ".\bicep\webapp-frontdoor.bicep" `
+  -appName "myapp" `
+  -frontDoorEndpointPrefix "myapp-afd"
+
+# Jenkins VM hardened: SSH key only, NSG restringida a tu IP
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $rg `
+  -TemplateFile ".\bicep\jenkins.bicep" `
+  -adminSshPublicKey (Get-Content "$HOME\.ssh\id_rsa.pub" -Raw) `
+  -allowedSshSourceCidr "<publicIP>/32"
